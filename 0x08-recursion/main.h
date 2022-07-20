49a4e867e06a19a3f7a9d31a9266bc8b2518daed
Sunky19 <timothy.ola886@gmail.com>
2022-07-20 06:32:48 -0700

    put recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+/**
+ *void prototypes(void)
+ *int prototypes(int)
+ *char prototypes(char) 
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
