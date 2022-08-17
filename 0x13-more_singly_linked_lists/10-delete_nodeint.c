cb1a2872333faa024ea4bc01c4a822457bae8a47
Sunky19 <timothy.ola886@gmail.com>
2022-08-16 22:12:40 -0700

    delete nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *old_node = NULL;
 	listint_t *previous_node = NULL;
 	unsigned int i = 0, list_len = listint_len(*head);
-	
+
 	if ((index > list_len) || (list_len == 0))
 		return (-1);
 	while (head != NULL)
